2513d1aadab6c7c039e781b5aae0c7bb9d410d00
LiuYu <735935388@qq.com>
2016-03-01 10:29:31 +0800

    新建项目

---
+++ 09News/09News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  09News
+//
+//  Created by LiuYu on 16/3/1.
+//  Copyright © 2016年 LiuYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 09News/09News/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  09News
+//
+//  Created by LiuYu on 16/3/1.
+//  Copyright © 2016年 LiuYu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
